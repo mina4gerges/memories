3f1ceeaccd6a1f91c07bc5467b80587c94247159
mina gerges <mingerges4@gmail.com>
2020-12-31 01:03:05 +0200

    remove node_modules

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js'
+import {CONNECTION_URL} from "./secrets.js";
 
 const app = express();
 
@@ -12,8 +13,6 @@ app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
 app.use(cors());
 
 app.use("/posts", postRoutes);
-
-const CONNECTION_URL = "mongodb+srv://mina-gerges:Mina@G20@cluster0.folfq.mongodb.net/Cluster0?retryWrites=true&w=majority";
 
 const PORT = process.env.PORT || 5000;
 
