fab4160c4f07667d8f543f3cf76b0ea3b12fc71e
mina gerges <mingerges4@gmail.com>
2021-01-18 20:50:59 +0200

    update url api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = "https://memories-project-youtube.herokuapp.com/posts";
+const url = 'https://memories-project-youtube.herokuapp.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const addPost = newPost => axios.post(url, newPost);
